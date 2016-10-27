a767e00747ec4c9195094220fa3b2453e92db44b
Aaron Endsley <Aaron Endsley>
2016-10-27 13:29:06 -0500

    started getting objests

--- js/matchstrings.js
+++ js/matchstrings.js
@@ -1,3 +1,19 @@
 /**
  * Created by aaron on 10/27/2016.
  */
+var submit=function (){
+ var stringToGet=$('#stringsToLookFor').val(),listOfStringsToSearch = $('#listOfStrings').val(), stringToGetArray=stringToGet.split(' '), stringToLookThrough=listOfStringsToSearch.split(' '), stringToGetObject={}, listOfStringsToSearchObject={}, matches={};
+
+ function moveTheArrayElementsToObjects(){
+  for(let i = 0; i < stringToGetArray.length; i++){
+    stringToGetObject[stringToGetArray[i]]=stringToGetArray[i];
+  };
+
+  for(let i = 0; i < stringToLookThrough.length; i++){
+      listOfStringsToSearchObject[stringToLookThrough[i]]=stringToLookThrough[i];
+  }
+
+ }
+ moveTheArrayElementsToObjects();
+
+};
