43e88f01fa7602c51daba1afc2c82d1a2b6a9859
Aaron Endsley <Aaron Endsley>
2016-10-27 15:55:32 -0500

    finished the core elements of the function

--- js/matchstrings.js
+++ js/matchstrings.js
@@ -2,18 +2,49 @@
  * Created by aaron on 10/27/2016.
  */
 var submit=function (){
- var stringToGet=$('#stringsToLookFor').val(),listOfStringsToSearch = $('#listOfStrings').val(), stringToGetArray=stringToGet.split(' '), stringToLookThrough=listOfStringsToSearch.split(' '), stringToGetObject={}, listOfStringsToSearchObject={}, matches={};
-
+ var stringToGet=$('#stringsToLookFor').val(),listOfStringsToSearch = $('#listOfStrings').val(), stringToGetArray, stringToLookThrough, stringToGetObject={}, listOfStringsToSearchObject={}, matches={};
+    function checkStrings() {
+        if(stringToGet !== ''){
+            stringToGetArray=stringToGet.split(' ');
+        }
+        if(listOfStringsToSearch !== ''){
+            stringToLookThrough=listOfStringsToSearch.split(' ');
+        }
+    }
  function moveTheArrayElementsToObjects(){
   for(let i = 0; i < stringToGetArray.length; i++){
     stringToGetObject[stringToGetArray[i]]=stringToGetArray[i];
   };
-
   for(let i = 0; i < stringToLookThrough.length; i++){
       listOfStringsToSearchObject[stringToLookThrough[i]]=stringToLookThrough[i];
   }
-
  }
- moveTheArrayElementsToObjects();
+ function checkForMatchesAndPush() {
+     for(let string in listOfStringsToSearchObject){
+         for(let secondString in stringToGetObject){
+             if(listOfStringsToSearchObject[string] === stringToGetObject[secondString]){
+                 matches[stringToGetObject[secondString]]=stringToGetObject[secondString];
+             }
+         }
+     }
+ }
+  function printObjectsToScreen(){
+            for(let match in matches){
+                $('#output').append('<p>' + matches[match] + '</p>')
+            }
+  }
+
+        checkStrings();
+        if(stringToGetArray) {
+            if(stringToLookThrough) {
+                moveTheArrayElementsToObjects();
+                checkForMatchesAndPush();
+                printObjectsToScreen();
+            }
+        }else{
+            $('#output').append('<p>Please enter a string</p>');
+        }
+
+    matches={};
 
 };
